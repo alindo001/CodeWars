1620308b5325740897b4b6fc1d1c3fcfadecc75d
Andres <andreslindo20@gmail.com>
2022-04-10 00:38:54 -0400

    codewars

--- nameShuffle.js
+++ nameShuffle.js
@@ -1,3 +1,13 @@
 // Write a function that returns a string in which firstname is swapped with last name.
 
 //  nameShuffler('john McClane'); => "McClane john" //
+
+
+
+function nameShuffler(str){
+    str = str.split(" ")
+    let str1 = str[1] +" "+ str [0]
+    return str
+ }
+
+ console.log(nameShuffler("John McClane"))
