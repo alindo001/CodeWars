8cd7f89a2c726c549e171bb50c00b17d6c13aee6
alindo <andreslindo20@gmail.com>
2022-04-13 21:07:30 -0400

    codewars

--- Tosquare(root)ornottosquare(root).js
+++ Tosquare(root)ornottosquare(root).js
@@ -3,3 +3,12 @@
 // Return a new array with processing every number of the input-array like this:
 
 // If the number has an integer square root, take this, otherwise square the number.
+
+
+function squareOrSquareRoot(array) {
+    return array.map(x => {
+      const r = Math.sqrt(x);
+      return (r % 1 == 0) ? r : (x*x);
+    });  
+  }
+  
