fc31bcc6d4a292874909b6d4d507b3486c55fc81
Andres <andreslindo20@gmail.com>
2022-05-11 13:57:10 -0400

    codewars

--- dryingPotatoes.js
+++ dryingPotatoes.js
@@ -18,12 +18,15 @@
 // int parameter p0 - initial percent of water-
 // int parameter w0 - initial weight -
 // int parameter p1 - final percent of water -
-// potatoesshould return the final weight coming out of the oven w1 truncated as an int.
+// potatoes should return the final weight coming out of the oven w1 truncated as an int.
 
 // Example:
 // potatoes(99, 100, 98) --> 50
 
 
 function potatoes(p0, w0, p1) {
-    // your code
+    w1 = 0
+    waterContent = (.01 * p0) * w0
+    return w1
+
 }
