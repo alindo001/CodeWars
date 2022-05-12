b9f351a60a4f27fc32d7269198ef840a8ef06193
Andres <andreslindo20@gmail.com>
2022-05-12 13:08:28 -0400

    codewars

--- oddOrEvenDeterminThat.js
+++ oddOrEvenDeterminThat.js
@@ -14,3 +14,16 @@
 // const EITHER = "Either";
 // is Preloaded.
 
+function oddOrEven(n) {
+    if (n !== 1){
+      if (n % 2 === 1) {
+        return "Either"
+      }
+      if (n/2 % 2 === 0) {
+        return "Even"
+      } else {
+        return "Odd"
+      }
+    }
+    return "Either"  
+  }
