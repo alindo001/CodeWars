8c905ac57073f82a91c0b283a6dd4ed924e38e03
Andres <andreslindo20@gmail.com>
2022-05-18 13:30:11 -0400

    CW

--- square(n)Sum.js
+++ square(n)Sum.js
@@ -2,6 +2,9 @@
 
 // For example, for [1, 2, 2] it should return 9 because 1^2 + 2^2 + 2^2 = 9.
 
+
 function squareSum(numbers){
-  
-}
+    return numbers.reduce((s,v) => s + v * v, 0);
+  }
+
+console.log(squareSum([1,2,2]))
