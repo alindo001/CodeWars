e2df7e5fb861f6dd4574ad439bc4ec02b354cdac
alindo <andreslindo20@gmail.com>
2022-05-21 23:21:49 -0400

    codewars

--- sumOrDifferencesInArray.js
+++ sumOrDifferencesInArray.js
@@ -8,3 +8,10 @@
 // Sum: (10 - 2) + (2 - 1) = 8 + 1 = 9
 
 // If the array is empty or the array has only one element the result should be 0 ( Nothing in Haskell ).
+
+
+
+const sumOfDifferences = arr => arr
+  .sort((a, b) => b - a)
+  .map((a, i) => a - arr[i + 1] || 0)
+  .reduce((a, b) => a + b, 0);
