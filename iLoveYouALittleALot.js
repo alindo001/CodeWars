c8418834b831e28dbbb833a38a5cee7cb7e20b7c
Andres <andreslindo20@gmail.com>
2022-05-25 23:58:01 -0400

    codewars

--- iLoveYouALittleALot.js
+++ iLoveYouALittleALot.js
@@ -9,3 +9,27 @@
 // When the last petal was torn there were cries of excitement, dreams, surging thoughts and emotions.
 
 // Your goal in this kata is to determine which phrase the girls would say for a flower of a given number of petals, where nb_petals > 0.
+
+
+
+function howMuchILoveYou(nbPetals) {
+    var muchLoves = [
+      'I love you',
+      'a little',
+      'a lot',
+      'passionately',
+      'madly',
+      'not at all'
+    ];
+    return muchLoves[(nbPetals - 1) % muchLoves.length];
+}
+// 1,7,13,19,25,31.37
+// 2,8,14,20,26,32,38
+// 3,9,15,21,27,33,39
+// 4,10,16,22,28,34,40
+
+
+console.log(howMuchILoveYou(1))
+console.log(howMuchILoveYou(7))
+console.log(howMuchILoveYou(15))
+console.log(howMuchILoveYou(268))
