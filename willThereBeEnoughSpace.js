00f9808fa7b4f2096cf435d726fc29afb10de09f
Andres <andreslindo20@gmail.com>
2022-07-11 00:10:33 -0400

    codewars

--- willThereBeEnoughSpace.js
+++ willThereBeEnoughSpace.js
@@ -12,3 +12,13 @@
 // Usage Examples:
 // cap = 10, on = 5, wait = 5 --> 0 # He can fit all 5 passengers
 // cap = 100, on = 60, wait = 50 --> 10 # He can't fit 10 of the 50 waiting
+
+
+function enough(capacity, onBus, waiting) {
+    let availableSeats = capacity - onBus
+    if (availableSeats - waiting >= 0){
+      return 0
+    }else{
+      return Math.abs(availableSeats - waiting)
+    }
+  }
