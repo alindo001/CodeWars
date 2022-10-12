d5ecf8398bdfb6f44e8857867666b090d3b4c4a0
Andres <andreslindo20@gmail.com>
2022-10-12 14:43:26 -0400

    codewars solution

--- extractTheDomainNameFromAURL.js
+++ extractTheDomainNameFromAURL.js
@@ -3,3 +3,19 @@
 // * url = "http://github.com/carbonfive/raygun" -> domain name = "github"
 // * url = "http://www.zombie-bites.com"         -> domain name = "zombie-bites"
 // * url = "https://www.cnet.com"                -> domain name = cnet"
+
+
+function parsesURL(url){
+    url = url.replace("//", " ").replace("www.", "").replace(".com"," ").replace(".","").replace("http:","").replace("https:","")
+
+    if(url.split(" ")[0]===""){
+        return url.split(" ")[1]
+    }else{
+        return url.split(" ")[0]
+    }
+}
+
+console.log(parsesURL("http://github.com/carbonfive/raygun")) // >> "github"
+console.log(parsesURL("http://www.zombie-bites.com"))   // >> "zombie-bites"
+console.log(parsesURL("https://www.cnet.com"))          // >> "cnet"
+console.log(parsesURL("www.xakep.ru"))
